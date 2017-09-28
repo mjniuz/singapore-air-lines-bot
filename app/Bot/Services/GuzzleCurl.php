b35ee49c25b7ffa911c23d794e11ad01ab0fa344
arywidiantara <arywidiantara33@gmail.com>
2017-09-28 22:52:18 +0700

    [ADD] documentation in guzzle

--- app/Bot/Services/GuzzleCurl.php
+++ app/Bot/Services/GuzzleCurl.php
@@ -2,9 +2,6 @@
 
 use GuzzleHttp\Client as GuzzleHttpLibrary;
 
-/**
- *
- */
 class GuzzleCurl extends GuzzleHttpLibrary
 {
     const METHOD_POST = "post";
@@ -21,11 +18,21 @@ class GuzzleCurl extends GuzzleHttpLibrary
 
     }
 
+    /**
+     * this function set endpoint for hit API from guzzle
+     *
+     * @param object $endpoint The endpoint
+     */
     protected function setEndpoint($endpoint)
     {
         $this->endpoint = $endpoint;
     }
 
+    /**
+     * this function for set header for hit API from guzzle
+     *
+     * @param object $headerArray The header array
+     */
     protected function setHeaders($headerArray)
     {
         if (is_object($headerArray))
@@ -34,16 +41,31 @@ class GuzzleCurl extends GuzzleHttpLibrary
         }
     }
 
+    /**
+     * this function for set body for hit API from guzzle
+     *
+     * @param object $body The body
+     */
     protected function setBody($body)
     {
         $this->body = $body;
     }
 
+    /**
+     * this function for set method
+     *
+     * @param GuzzleCurl $method The method
+     */
     protected function setMethod(GuzzleCurl $method)
     {
         $this->method = $method;
     }
 
+    /**
+     * this function for hit API from guzzle
+     *
+     * @return object
+     */
     protected function send()
     {
         $request = $this->client->$method($this->endpoint, $this->headers, array());
