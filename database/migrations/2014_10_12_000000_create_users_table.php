f98e7366240b3d29645df0edd018b081d75777de
arywidiantara <arywidiantara33@gmail.com>
2017-09-29 09:29:09 +0700

    [UPDATE] change migrations and add model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,13 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table)
         {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email');
-            $table->string('password');
+            $table->string('full_name')->nullable();
+            $table->string('username')->nullable();
+            $table->string('password')->nullable();
+            $table->string('profile_picture')->nullable();
+            $table->string('facebook_id')->nullable();
+            $table->string('telegram_id')->nullable();
+            $table->string('line_id')->nullable();
             $table->string('token');
             $table->rememberToken();
             $table->timestamps();
