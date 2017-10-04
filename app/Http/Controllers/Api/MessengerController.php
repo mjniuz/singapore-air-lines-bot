de4ab3fdf40caabbe7948503418a943753226c47
Alan el <mail@mjniuz.com>
2017-10-04 13:25:54 +0700

    update message

--- app/Http/Controllers/Api/MessengerController.php
+++ app/Http/Controllers/Api/MessengerController.php
@@ -53,7 +53,7 @@ class MessengerController extends ApiController
         if (isset($data['entry'][0]['messaging'][0]['sender']['id']))
         {
             $facebook_id    = $data['entry'][0]['messaging'][0]['sender']['id'];
-            $sender_message = $data['entry'][0]['messaging'][0]['message']['text'];
+            $message        = $data['entry'][0]['messaging'][0]['message']['text'];
 
             // get detail user
             $user   = $this->messenger_repository->getDetailMember($facebook_id);
@@ -64,17 +64,18 @@ class MessengerController extends ApiController
             }
 
             // check if sender messege if not empty
-            if (!empty($sender_message))
+            if (!empty($message))
             {
-                $message = $this->request_repository->handlingRequest($sender_message, $facebook_id);
+                /*$message = $this->request_repository->handlingRequest($sender_message, $facebook_id);
                 if (empty($message))
                 {
                     $message = $this->request_repository->handlingErrorFormat();
-                }
+                }*/
                 // create log
                 $this->messenger_repository->create($user->id,'text', $message);
                 $response = $this->messenger_repository->sendTextMessage($facebook_id, $message);
                 Log::error(json_encode($response));
+
 
                 // return message
                 return response()->json(['message' => $message, 'response' => $response], 200);
