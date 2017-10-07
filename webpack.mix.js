c0c5cff3d16af1943f2a60d47843fd3dbfa1ffbe
arywidiantara <arywidiantara33@gmail.com>
2017-10-07 09:55:58 +0700

    [UPDATE] fix for npm production

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,12 +15,14 @@ let mix = require('laravel-mix');
 //    .sass('resources/assets/sass/app.scss', 'public/css');
 
 mix.js([
-    'assets/js/app.min.js',
-    'assets/js/bootstrap.min.js',
-    'assets/js/bootstrap-timepicker.min.js',
+    'resources/assets/js/jquery.min.js',
+    'node_modules/jquery/dist/jquery.min.js',
+    // 'resources/assets/js/app.min.js',
+    'resources/assets/js/bootstrap.min.js',
+    'resources/assets/js/bootstrap-timepicker.min.js',
     'resources/assets/js/front.js',
     'node_modules/sweetalert2/dist/sweetalert2.min.js',
-    'resources/assets/js/main.js'
+    'resources/assets/js/main.js',
 ], 'public/js/app.min.js');
 mix.combine([
     'node_modules/sweetalert2/dist/sweetalert2.min.css',
