0d945256db02128391893a80a966c59c99e063c6
arywidiantara <arywidiantara33@gmail.com>
2017-10-07 13:02:26 +0700

    [UPDATE] fix for sweetalert for set

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -59,7 +59,11 @@
         @yield('content')
     </div>
 
-    {{-- <script src="{{ asset('assets/js/app.min.js') }}"></script> --}}
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <script src="{{asset('assets/js/app.min.js')}}"></script>
+        <script src="{{asset('assets/js/sweetalert-dev.js') }}"></script>
+        <script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
     <script src="{{ asset('js/app.min.js') }}"></script>
     @yield('script')
 </body>
