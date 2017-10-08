64202ac14e84807ab52b7f8e1724f739640dfdc3
Alan el <mail@mjniuz.com>
2017-10-08 08:09:48 +0700

    update flight random

---
+++ app/Bot/Repository/FlightRepository.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Bot\Repository;
+
+
+class FlightRepository extends Repository
+{
+    public function randomData($price = 0){
+        /*
+         "id" => 10
+          "from_location" => "jakarta"
+          "from_code" => "CGK"
+          "from_airport" => "Soekarno–Hatta International Airport"
+          "to_location" => "Singapore"
+          "to_code" => "SIN"
+          "to_airport" => "Changi Airport"
+          "date" => "2017-10-13 20:00:00"
+          "amount_found" => 440000
+          "travel_time" => 290
+          "created_at" => null
+          "updated_at" => null
+         */
+        if($price == 0){
+            $price  = 100;
+        }
+        $data   = [];
+        for($i = 1; $i < 6; $i++){
+            if($i != 1){
+                $price  = ($price + $i + rand(1,10));
+            }
+            $data[] =   [
+                "id"    => $i,
+                "from_location" => "Jakarta",
+                "from_code"     => "CGK",
+                "from_airport"  => "Soekarno–Hatta International Airport",
+                "to_location"   => "Singapore",
+                "to_code"       => "SIN",
+                "to_airport"    => "Changi Airport",
+                "amount_found"  => $price,
+                "only_time"     => date("H:i", strtotime("+" . $i . " hours")),
+                "only_time_arrival" => date("H:i", strtotime("+" . ($i + 1) . " hours")),
+                "convert_time"  => "01 Hours"
+            ];
+        }
+
+        return json_decode(json_encode($data));
+    }
+}
